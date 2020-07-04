bd999bc6b0cfe6f2deac6a179c6470544f29f815
Chidi Chukwu <34505544+Chidi-chukwu@users.noreply.github.com>
2020-07-04 15:05:04 +0100

    first changes

---
+++ example.js
@@ -0,0 +1,9 @@
+/*const para = document.querySelectorAll('p');
+para.forEach(para =>{
+  if (para.textContent.includes('you')){
+  para.classList.add('white')
+  para.style.backgroundColor = 'brown';
+  para.style.textAlign = 'center'
+  /*para.innerText = 'Jesus is my Lord and personal saviour'*/
+}
+})*/
